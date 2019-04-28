e45f0c393b74ce94fd5579baede378be7751e2aa
leaosunday <leaosunday25@gmail.com>
2019-04-28 16:16:12 +0800

    finish gradient descent chapter

--- 06-gradient-descent/playML/linear_regression.py
+++ 06-gradient-descent/playML/linear_regression.py
@@ -46,6 +46,16 @@ class LinearRegression:
             # return res * 2 / len(X_b)
             return X_b.T.dot(X_b.dot(theta) - y) * 2. / len(X_b) # vectorize
 
+        def dJ_debug(theta, X_b, y, epsilon=0.01):
+            res = np.empty(len(theta))
+            for i in range(len(theta)):
+                theta_1 = theta.copy()
+                theta_1[i] += epsilon
+                theta_2 = theta.copy()
+                theta_2[i] -= epsilon
+                res[i] = (J(theta_1, X_b, y) - J(theta_2, X_b, y)) / (2 * epsilon)
+            return res
+
         def gd(X_b, y, initial_theta, eta, n_iters=1e4, epsilon=1e-8):
             theta = initial_theta
             cur_iter = 0
